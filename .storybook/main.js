adc09055c2230972d14cc6f374be2fb62fbff42d
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-31 13:51:14 -0300

    update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,25 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/vue3',
+  webpackFinal: (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+
+    return config
+  },
 }
