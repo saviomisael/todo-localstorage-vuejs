cc83a961de47fac996d7fe0af68d0a8c4c27cda4
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-02-01 19:19:03 -0300

    refactor TodoList

--- src/components/TodoList/script.js
+++ src/components/TodoList/script.js
@@ -7,9 +7,26 @@ export default {
       type: Array,
       required: true,
     },
+    mode: {
+      type: String,
+      default() {
+        return 'LIST_UNDONE'
+      },
+      validator(value) {
+        return value === 'LIST_UNDONE' || value === 'LIST_DONE'
+      },
+    },
   },
   components: {
     TodoItem,
+  },
+  computed: {
+    showTodos() {
+      return this.todos.length
+    },
+    showEmptyMessage() {
+      return this.mode === 'LIST_UNDONE' && !this.todos.length
+    },
   },
   methods: {
     handleCheckBoxClick(todoId) {
